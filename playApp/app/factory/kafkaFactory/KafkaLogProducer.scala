d828f4f6f59767196b99a51787525ef9a41a83d1
Himanshu Arora <harora.prestataire@si2m.com>
2017-04-09 18:01:24 +0200

    unnecessay lazy removed & readme updated

--- playApp/app/factory/kafkaFactory/KafkaLogProducer.scala
+++ playApp/app/factory/kafkaFactory/KafkaLogProducer.scala
@@ -9,18 +9,18 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
   */
 object KafkaLogProducer {
 
-  private lazy val configuration = play.Configuration.root()
+  private val configuration = play.Configuration.root()
 
-  private lazy val props = new Properties
+  private val props = new Properties
   props.put("bootstrap.servers", configuration.getString("kafka.bootstrap.servers"))
   props.put("key.serializer", configuration.getString("kafka.serializer.class"))
   props.put("value.serializer", configuration.getString("kafka.serializer.class"))
   props.put("acks", configuration.getString("kafka.acks"))
   props.put("reconnect.backoff.ms", configuration.getString("kafka.reconnect.backoff.ms"))
 
-  private lazy val topicName = configuration.getString("kafka.topicName")
+  private val topicName = configuration.getString("kafka.topicName")
 
-  private lazy val producer = new KafkaProducer[String, String](props)
+  private val producer = new KafkaProducer[String, String](props)
 
   def send(msg: String) = producer.send(new ProducerRecord[String, String](topicName, msg))
 
